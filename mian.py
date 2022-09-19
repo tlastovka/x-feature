b46b24e573b63818acf87952cf36b82a69d3d662
Tomas <tl@premiato.org>
2022-09-19 18:07:24 +0200

    My first commit

---
+++ mian.py
@@ -0,0 +1,18 @@
+name = "Jamil"
+name2 = "Jamila"
+
+def hello(name: str):
+    x = print(name.upper())
+
+
+def hello2(name2: str):
+    print(name2.upper())
+
+
+if __name__ == '__main__':
+    # TODO: ADD FORMATING
+
+    print("Python is Awesome 3")
+    print("python is awesome 3")
+    print("python is awesome 3")
+    
